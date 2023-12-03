ca3b06fdfa5800dc33e4d3073cd089258b4d90aa
maytinhdibo <iammaytinhdibo@gmail.com>
2023-12-03 14:18:45 +0100

    alioth: Re-Add Battery Friendly Pocket Mode
    * Adapt to A14

--- device.mk
+++ device.mk
@@ -273,6 +273,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Platform
 TARGET_BOARD_PLATFORM := kona
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    PocketMode
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/pocket/privapp-permissions-pocketmode.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-pocketmode.xml
+
 # QCRIL
 PRODUCT_VENDOR_PROPERTIES += \
     persist.vendor.radio.cdma_cap=true \
