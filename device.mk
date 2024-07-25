7b2220042e2ad24557b4c9fca9e6d34164781a11
johnmart19 <johnivan19999@gmail.com>
2024-07-25 17:21:40 +0800

    Alioth: Inherit TouchFeature Type
    - From Miui Alioth

--- device.mk
+++ device.mk
@@ -347,6 +347,10 @@ PRODUCT_VENDOR_PROPERTIES += \
 PRODUCT_VENDOR_PROPERTIES += \
     vendor.sys.thermal.data.path=/data/vendor/thermal/
 
+# TouchFeature
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.touchfeature.type=43
+
 # Update Engine
 PRODUCT_PACKAGES += \
     update_engine \
